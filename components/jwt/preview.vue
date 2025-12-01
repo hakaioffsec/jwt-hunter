ffd23eb1e8edfdf2428b20738b66d07b093cf007
Murilo Caixeta <murilocaixeta.udi@gmail.com>
2025-12-01 13:34:01 -0300

    fix(mobile): Responsive design for mobile

--- components/jwt/preview.vue
+++ components/jwt/preview.vue
@@ -1,26 +1,82 @@
 <template>
-	<div class="flex flex-row space-x-8 mt-14 w-full justify-center">
-		<div class="col-span-1 md:col-span-1 flex flex-col">
-			<label for="encoded-token" class="block text-left text-white font-normal pb-4 text-2xl">Encoded</label>
-			<textarea v-model="form.token" id="jwt-token" variant="none" :cols=50 spellcheck="false" class="text-white text-md h-full p-4 border border-secondary-300 bg-secondary focus:outline-none focus:border-secondary-300 rounded-md resize-none" />
+	<div
+		class="flex flex-col md:flex-row md:space-x-8 space-y-8 md:space-y-0 mt-14 w-full justify-center px-4"
+	>
+		<div class="w-full md:w-auto flex flex-col">
+			<label for="encoded-token" class="block text-left text-white font-normal pb-4 text-2xl">
+				Encoded
+			</label>
+
+			<textarea
+				v-model="form.token"
+				id="jwt-token"
+				variant="none"
+				:cols="50"
+				spellcheck="false"
+				class="w-full md:w-auto text-white text-md h-64 md:h-full p-4 border border-secondary-300 bg-secondary
+					   focus:outline-none focus:border-secondary-300 rounded-md resize-none"
+			/>
 		</div>
-		<div class="flex flex-col flex-1">
-			<label for="header" class="block text-left text-white font-normal pb-4 text-2xl">Decoded</label>
-			<div id="header"
-				class="flex flex-col w-full divide-y divide-secondary-300 border border-secondary-300 bg-secondary rounded-md shadow-md text-lg flex-1"
-				spellcheck="false">
+
+		<div class="flex flex-col flex-1 w-full">
+			<label for="header" class="block text-left text-white font-normal pb-4 text-2xl">
+				Decoded
+			</label>
+
+			<div
+				id="header"
+				class="flex flex-col w-full divide-y divide-secondary-300 border border-secondary-300
+					   bg-secondary rounded-md shadow-md text-lg flex-1"
+				spellcheck="false"
+			>
 				<div class="flex py-1.5 px-3">
-					<span class="text-white text-sm">HEADER: <span class="text-gray-500">ALGORITHM & TOKEN TYPE</span></span>
+					<span class="text-white text-sm">HEADER:
+						<span class="text-gray-500">ALGORITHM & TOKEN TYPE</span>
+					</span>
 				</div>
-				<UTextarea id="jwt-token-header" autoresize v-model="jwtParts.header" variant="none" :rows=5 spellcheck="false" textareaClass="text-md text-jwt-header p-4 rounded-none border border-secondary focus:border-secondary"/>
+
+				<UTextarea
+					id="jwt-token-header"
+					autoresize
+					v-model="jwtParts.header"
+					variant="none"
+					:rows="5"
+					spellcheck="false"
+					textareaClass="text-md text-jwt-header p-4 rounded-none border border-secondary focus:border-secondary w-full"
+				/>
+
 				<div class="flex py-1.5 px-3">
-					<span class="text-white text-sm">PAYLOAD: <span class="text-gray-500">DATA</span></span>
+					<span class="text-white text-sm">PAYLOAD:
+						<span class="text-gray-500">DATA</span>
+					</span>
 				</div>
-				<UTextarea id="jwt-token-payload" autoresize v-model="jwtParts.payload" variant="none" spellcheck="false" :rows="8" textareaClass="text-md text-jwt-payload p-4 rounded-t-none border border-secondary focus:border-secondary"/>
+
+				<UTextarea
+					id="jwt-token-payload"
+					autoresize
+					v-model="jwtParts.payload"
+					variant="none"
+					spellcheck="false"
+					:rows="8"
+					textareaClass="text-md text-jwt-payload p-4 rounded-t-none border border-secondary focus:border-secondary w-full"
+				/>
+
 				<div v-if="form.attack == EAttacks.CUSTOM" class="flex py-1.5 px-3">
-					<span class="text-white text-sm">SECRET: <span class="text-gray-500">KEY TO SIGN TOKEN <span v-if="verifyAsymmetricAlg()">(RSA KEY)</span></span></span>
+					<span class="text-white text-sm">SECRET:
+						<span class="text-gray-500">
+							KEY TO SIGN TOKEN
+							<span v-if="verifyAsymmetricAlg()">(RSA KEY)</span>
+						</span>
+					</span>
 				</div>
-				<UTextarea v-if="form.attack == EAttacks.CUSTOM" v-model="jwtParts.secret" variant="none" spellcheck="false" textareaClass="text-md text-white p-4 rounded-t-none border border-secondary focus:border-secondary"/>
+
+				<UTextarea
+					v-if="form.attack == EAttacks.CUSTOM"
+					v-model="jwtParts.secret"
+					variant="none"
+					spellcheck="false"
+					textareaClass="text-md text-white p-4 rounded-t-none border border-secondary focus:border-secondary w-full"
+				/>
 			</div>
 		</div>
 	</div>
